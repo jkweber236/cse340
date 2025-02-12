a896e08c67aafde6aedb93819aaddd8795102dcc
Jennifer <coo21040@byui.edu>
2025-02-12 00:04:23 -0700

    Completed Account: Process Registration

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,4 +1,5 @@
 const utilities = require("../utilities/index")
+const accountModel = require("../models/account-model")
 
 /* ****************************************
 *  Deliver login view
@@ -17,9 +18,41 @@ async function buildLogin(req, res, next) {
 async function buildRegister(req, res, next) {
    let nav = await utilities.getNav()
    res.render("account/register", {
-     title: "Register",
-     nav,
+      title: "Register",
+      nav,
    })
- }
+}
 
-module.exports = { buildLogin, buildRegister }
+ /* ****************************************
+*  Process Registration
+* *************************************** */
+async function registerAccount(req, res) {
+   let nav = await utilities.getNav()
+   const { account_firstname, account_lastname, account_email, account_password } = req.body
+
+   const regResult = await accountModel.registerAccount(
+      account_firstname,
+      account_lastname,
+      account_email,
+      account_password
+   )
+
+   if (regResult) {
+      req.flash(
+         "notice",
+         `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+      )
+      res.status(201).render("account/login", {
+         title: "Login",
+         nav,
+      })
+   } else {
+      req.flash("notice", "Sorry, the registration failed.")
+      res.status(501).render("account/register", {
+         title: "Registration",
+         nav,
+      })
+   }
+}
+
+module.exports = { buildLogin, buildRegister, registerAccount }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,7 +3,13 @@ const router = new express.Router()
 const utilities = require("../utilities/index")
 const accountController = require("../controllers/accountController")
 
+// Build login
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+// Build registration
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+// Register account
+router.post("/register", utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router;
