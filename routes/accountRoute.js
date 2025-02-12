62efa132a863e88f9a666d5dd9ffc5c0731e2c15
Jennifer <coo21040@byui.edu>
2025-02-11 19:18:36 -0700

    Completed deliver registration view

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,5 +4,6 @@ const utilities = require("../utilities/index")
 const accountController = require("../controllers/accountController")
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 module.exports = router;
